6774e510d5be7f824dce0081c7e4ca0ae200a43f
Home <home@192.168.1.126>
2023-11-16 18:36:30 +0000

    Latest Development

---
+++ app/guest.py
@@ -0,0 +1,40 @@
+class Guest:
+    "This class represents a room in a hotel, we need to implement enumerate type, fro size of hotel room, must be single, double, tripple"
+    name = ''
+    age = ''
+    nationality = ''
+    mobile = 0
+    id = False
+    
+    def __init__(self, name, age, nationality, mobile, id) :
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+        self.mobile = mobile
+        self.id = id
+        
+    def getGuestInfo(self): 
+        details = "Guest Name: " + str(self.getName()) + ", " + self.name + "'s age is: " + str(self.getAge()) + ", The Guest is from: " + str(self.getNationality()) + ", Mobile Number: " + str(self.getMobile()) + ", Is there ID: " + str(self.getId())  + "."
+        return details
+    
+    def getAge(self):
+        return self.age
+    
+    def getName(self):
+        return self.name
+    
+    def getMobile(self):
+        return self.mobile
+    
+    def getId(self):
+        return self.id
+    
+    def getNationality(self):
+        return self.nationality
+    
+    
+Saed_Guest = Guest("Saed", 17, "England", 7399022238, True)
+
+print(Saed_Guest.getGuestInfo())
+
+
