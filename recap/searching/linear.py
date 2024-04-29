5012cd2a17fd874d639b9ff771f462df48278a0f
saed <65547726+farah-sm@users.noreply.github.com>
2024-04-29 20:15:42 +0100

    Create linear.py

---
+++ recap/searching/linear.py
@@ -0,0 +1,12 @@
+from array import *
+
+numbers = [1, 4, 6, 8, 100, 1234]
+
+def linear(arr):
+    arr = [1, 4, 6, 8, 100, 1234]
+    value = 8
+    
+    for i in arr:
+        print(i)
+        
+linear([1, 4, 7, 9])
